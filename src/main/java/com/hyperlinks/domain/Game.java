e9eb8036ab28e4b45d74cec151dc962259ac1f63
noahbr62 <noah.barth@edu.tbz.ch>
2020-12-11 10:43:25 +0100

    Refactoring

--- src/main/java/com/hyperlinks/domain/Game.java
+++ src/main/java/com/hyperlinks/domain/Game.java
@@ -2,14 +2,18 @@ package com.hyperlinks.domain;
 
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
-@Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Game extends AbstractEntity{
 
     @Column(nullable = false, unique = true)
--- src/main/java/com/hyperlinks/domain/User.java
+++ src/main/java/com/hyperlinks/domain/User.java
@@ -1,7 +1,9 @@
 package com.hyperlinks.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.ManyToOne;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class User extends AbstractEntity {
 
     @Column(nullable = false, unique = true)
