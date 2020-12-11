5af0bba77eb8542619ba0d8d393d4e748ec3a6e1
noahbr62 <noah.barth@edu.tbz.ch>
2020-12-11 10:31:16 +0100

    Refactored Game

--- src/main/java/com/hyperlinks/domain/Game.java
+++ src/main/java/com/hyperlinks/domain/Game.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Game extends AbstractEntity{
 
+    @Column(nullable = false, unique = true)
     private String inviteCode;
 
 }
