7ad4fe096c105c0e30e75f28e440da2734b6c5aa
noahbr62 <noah.barth@edu.tbz.ch>
2020-12-11 10:28:47 +0100

    Refactored Game

--- src/main/java/com/hyperlinks/domain/Game.java
+++ src/main/java/com/hyperlinks/domain/Game.java
@@ -1,4 +1,15 @@
 package com.hyperlinks.domain;
 
-public class Game {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game extends AbstractEntity{
+
+    private String inviteCode;
+
 }
