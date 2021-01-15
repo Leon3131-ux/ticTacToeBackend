c2f07e2bdf806f21b732aa95af6a6d1f3fdc62d6
alessandropisani <alessandro.pisani@bsi-software.com>
2021-01-15 08:37:36 +0100

    added doc for invite code generator

--- src/main/java/com/hyperlinks/util/InviteCodeGenerator.java
+++ src/main/java/com/hyperlinks/util/InviteCodeGenerator.java
@@ -4,6 +4,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class InviteCodeGenerator {
 
+    /**
+     * Returns an invite code that consists of an input {@link String} and a randomly generated {@code int} value
+     *
+     * @param   username            The username {@link String} the random code gets appended to
+     * @param   inviteCodeStrength  The length of the random code, must be between 0 and 9
+     * @return  A {@link String} consisting of username and appended random code
+     * @throws  IllegalArgumentException If {@code inviteCodeStrength} is more than 9 or less than 0
+     */
     public String generateInviteCode(String username, int inviteCodeStrength) {
         if(inviteCodeStrength == 0){
             return username;
