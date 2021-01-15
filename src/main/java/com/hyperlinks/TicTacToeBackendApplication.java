e5136112b96b9a9fc69ac96bfca05659e21de862
Michi202 <michael.marchesi@edu.tbz.ch>
2021-01-15 08:24:40 +0100

    added bean for invite code generator

--- src/main/java/com/hyperlinks/TicTacToeBackendApplication.java
+++ src/main/java/com/hyperlinks/TicTacToeBackendApplication.java
@@ -1,7 +1,9 @@
 package com.hyperlinks;
 
+import com.hyperlinks.util.InviteCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicTacToeBackendApplication {
@@ -9,5 +11,8 @@ public class TicTacToeBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeBackendApplication.class, args);
     }
+
+    @Bean
+    public InviteCodeGenerator inviteCodeGenerator() {return new InviteCodeGenerator();}
 
 }
