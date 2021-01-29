d9dc4f9e96d6a0fb12879cff5746aff4f9315f62
Michi202 <michael.marchesi@edu.tbz.ch>
2021-01-29 09:27:12 +0100

    bugfix

--- src/main/java/com/hyperlinks/controller/PlayerController.java
+++ src/main/java/com/hyperlinks/controller/PlayerController.java
@@ -4,11 +4,14 @@ import com.hyperlinks.converter.PlayerConverter;
 import com.hyperlinks.domain.Player;
 import com.hyperlinks.dto.SignUpDto;
 import com.hyperlinks.service.PlayerService;
+import com.hyperlinks.validator.SignUpValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +22,12 @@ public class PlayerController {
 
     private final PlayerService playerService;
     private final PlayerConverter playerConverter;
+    private final SignUpValidator signUpValidator;
+
+    @InitBinder("signUpDto")
+    public void initSaveProductBinder(WebDataBinder binder) {
+        binder.setValidator(signUpValidator);
+    }
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/signUp")
